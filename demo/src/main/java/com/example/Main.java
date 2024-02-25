4a5acc085c12e1cd4beb919ab7977ccf43ec1c82
unknown <devmethri96@gmail.com>
2024-02-25 17:01:45 +0530

    docker mistake

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dev Methri 96! HIiii");
+        System.out.println("Hello Dev Methri 96! HI");
     }
 }
